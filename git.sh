8d8f528f5af8b9fa12e270d598704d0de056a394
Leonid Dobrovolskyi <ldobrovols@e2r7p9.unit.ua>
2020-01-02 19:19:29 +0200

    исправлен баг в git.sh

--- git.sh
+++ git.sh
@@ -13,7 +13,8 @@ read -p "Press y to confirm" -n 1 -r choice
 
 if [[ $choice == "y" ]]
 then 
-	read -p "\nInput commit: " -r str
+	echo "\n"
+	read -p "Input commit: " -r str
 	git commit -m "$str"
 	git push
 fi
